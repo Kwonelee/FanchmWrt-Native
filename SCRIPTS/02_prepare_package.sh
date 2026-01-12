a5ec677d5819b38d9b5efd9389b4e99b18343aef
Kwonelee <995546054@qq.com>
2026-01-12 23:17:50 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -10,11 +10,6 @@ sed -i '/luci-app-ddns \\/d' include/target.mk
 # 更新 Feeds
 ./scripts/feeds update -a
 ./scripts/feeds install -a
-# 移除 SNAPSHOT 标签
-#sed -i 's,-SNAPSHOT,,g' include/version.mk
-#sed -i 's,-SNAPSHOT,,g' package/base-files/image-config.in
-#sed -i '/CONFIG_BUILDBOT/d' include/feeds.mk
-#sed -i 's/;)\s*\\/; \\/' include/feeds.mk
 
 ### 最后的收尾工作 ###
 # Lets Fuck
