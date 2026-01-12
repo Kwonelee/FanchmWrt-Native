e72c8b974275f41ae404e6108ac1c50f73a0e9b6
Kwonelee <995546054@qq.com>
2026-01-12 22:51:41 +0800

    Update 02_target_only.sh

--- SCRIPTS/M2/02_target_only.sh
+++ SCRIPTS/M2/02_target_only.sh
@@ -6,10 +6,10 @@ sed -i 's,-mcpu=generic,-march=armv8-a+crc+crypto,g' include/target.mk
 sed -i 's,kmod-r8168,kmod-r8169,g' target/linux/rockchip/image/armv8.mk
 
 #Vermagic
-#latest_version="$(curl -s https://github.com/openwrt/openwrt/tags | grep -Eo "v[0-9\.]+\-*r*c*[0-9]*.tar.gz" | sed -n '/[2-9]4/p' | sed -n 1p | sed 's/v//g' | sed 's/.tar.gz//g')"
-#wget https://downloads.openwrt.org/releases/${latest_version}/targets/rockchip/armv8/profiles.json
-#jq -r '.linux_kernel.vermagic' profiles.json >.vermagic
-#sed -i -e 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
+latest_version="$(curl -s https://github.com/fanchmwrt/fanchmwrt/tags | grep -Eo 'v[0-9]+\.[0-9]+\.[0-9]+' | head -n 1 | sed 's/^v//')"
+wget https://downloads.openwrt.org/releases/${latest_version}/targets/rockchip/armv8/profiles.json
+jq -r '.linux_kernel.vermagic' profiles.json >.vermagic
+sed -i -e 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
 
 # ============================================================================================================
 # 添加设备⬇⬇⬇
