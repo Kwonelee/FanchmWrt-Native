e69ca1abdfb04caea967f343ecb352574e8e12b3
Kwonelee <995546054@qq.com>
2026-01-12 23:12:38 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -3,7 +3,9 @@ clear
 
 ### 基础部分 ###
 # 使用 O2 级别的优化
-#sed -i 's/Os/O2/g' include/target.mk
+sed -i 's/Os/O2/g' include/target.mk
+sed -i '/luci-app-uhttpd \\/d' include/target.mk
+sed -i '/luci-app-ddns \\/d' include/target.mk
 # 更新 Feeds
 ./scripts/feeds update -a
 ./scripts/feeds install -a
