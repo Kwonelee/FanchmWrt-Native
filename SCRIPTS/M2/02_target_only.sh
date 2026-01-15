aa8d5945ba685962c1d1fb9d72299b5a999a2e55
Kwonelee <995546054@qq.com>
2026-01-15 13:21:50 +0800

    更新 02_target_only.sh

--- SCRIPTS/M2/02_target_only.sh
+++ SCRIPTS/M2/02_target_only.sh
@@ -72,7 +72,7 @@ git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt feeds/packa
 
 # zerotier
 rm -rf feeds/packages/net/zerotier
-#git clone https://github.com/sbwml/feeds_packages_net_zerotier feeds/packages/net/zerotier
+git clone https://github.com/sbwml/feeds_packages_net_zerotier feeds/packages/net/zerotier
 
 # 移除待替换插件
 rm -rf feeds/packages/net/adguardhome
