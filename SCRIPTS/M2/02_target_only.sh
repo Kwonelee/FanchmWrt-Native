03b7f10ab22805c2634e5974df11b718f97fdf95
Kwonelee <995546054@qq.com>
2026-04-01 09:49:29 +0800

    Update 02_target_only.sh

--- SCRIPTS/M2/02_target_only.sh
+++ SCRIPTS/M2/02_target_only.sh
@@ -3,7 +3,7 @@ clear
 
 # 使用特定的优化
 #sed -i 's,-mcpu=generic,-march=armv8-a+crc+crypto,g' include/target.mk
-sed -i 's,kmod-r8168,kmod-r8169,g' target/linux/rockchip/image/armv8.mk
+#sed -i 's,kmod-r8168,kmod-r8169,g' target/linux/rockchip/image/armv8.mk
 
 # Vermagic
 latest_version="$(curl -s https://github.com/fanchmwrt/fanchmwrt/tags | grep -Eo 'v[0-9]+\.[0-9]+\.[0-9]+' | head -n 1 | sed 's/^v//')"
