2163cffc1b8e8fd74fb87e389681967281ab0d3c
Kwonelee <995546054@qq.com>
2026-04-18 14:51:55 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -92,7 +92,7 @@ function git_sparse_clone() {
 # 常见插件
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 git_sparse_clone main https://github.com/gdy666/luci-app-lucky luci-app-lucky lucky
-替换 package/new/luci-app-lucky/
+替换 package/new/luci-app-lucky/luasrc/view/lucky/lucky_status.htm
 git_sparse_clone main https://github.com/sbwml/luci-app-openlist2 luci-app-openlist2 openlist2
 git_sparse_clone main https://github.com/sbwml/openwrt_pkgs luci-app-cpufreq luci-app-diskman luci-app-zerotier
 git_sparse_clone main https://github.com/Kwonelee/openwrt-packages luci-app-ramfree filebrowser luci-app-filebrowser-go
